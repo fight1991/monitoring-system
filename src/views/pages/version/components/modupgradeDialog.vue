67727a0b46c0577d72a244b0d7510c4cad0428a3
gao-jie-999 <386909546@qq.com>
2020-08-11 11:38:50 +0800

    中英文

--- src/views/pages/version/components/modupgradeDialog.vue
+++ src/views/pages/version/components/modupgradeDialog.vue
@@ -1,40 +1,40 @@
 <template>
   <el-dialog
     class="sys-dialog"
-    :title="'批量升级'"
+    :title="$t('invupgrade.upgrade')"
     :modal-append-to-body="false"
     @close="closeDialog"
     @open="getVersionInfo"
     :visible.sync="dialogVisible"
     width="550px">
-    <el-form size="mini" ref="dataForm" :model="dataForm" :rules="rules" label-width="100px">
+    <el-form size="mini" ref="dataForm" :model="dataForm" :rules="rules" label-width="120px">
       <el-row>
         <el-col :span="24">
-          <el-form-item label="任务名" prop="taskName">
+          <el-form-item :label="$t('invupgrade.taskName')" prop="taskName">
             <el-input v-model="dataForm.taskName" clearable></el-input>
           </el-form-item>
         </el-col>
         <el-col :span="24">
-          <el-form-item label="软件类别" prop="softType">
+          <el-form-item :label="$t('firmware.SoftType')" prop="softType">
             <el-select v-model="dataForm.softType" clearable style="width:100%">
               <el-option v-for="item in solfTypeList" :label="item" :value="item" :key="item"></el-option>
             </el-select>
           </el-form-item>
         </el-col>
         <el-col :span="24">
-          <el-form-item label="软件版本" prop="id">
+          <el-form-item :label="$t('invupgrade.version')" prop="id">
             <el-select v-model="dataForm.id" clearable style="width:100%">
               <el-option v-for="item in solfVersionList" :label="item.version" :value="item.id" :key="item.id"></el-option>
             </el-select>
           </el-form-item>
         </el-col>
         <el-col :span="24">
-          <el-form-item label="固件名称">
+          <el-form-item :label="$t('firmware.firmwareName')">
             <el-input v-model="nameNote.name" readonly></el-input>
           </el-form-item>
         </el-col>
         <el-col :span="24">
-          <el-form-item label="备注">
+          <el-form-item :label="$t('join.remark')">
             <el-input v-model="nameNote.note" readonly></el-input>
           </el-form-item>
         </el-col>
