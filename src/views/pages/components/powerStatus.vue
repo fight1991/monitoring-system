b6fb212121dac4950368c1c708331a521a8aed25
fight1991 <359193813@qq.com>
2020-09-15 12:05:51 +0800

    暂存

--- src/views/pages/components/powerStatus.vue
+++ src/views/pages/components/powerStatus.vue
@@ -24,15 +24,12 @@
       <el-card >
         <div class="title border-line" slot="header">{{$t('plant.baStatus')}}</div>
         <div class="g-container">
-          <div class="g-number">98.7%</div>
+          <div class="g-datas g-number">98.7%</div>
+          <div class="g-datas g-number">98.7%</div>
+          <div class="g-datas g-number">98.7%</div>
           <div class="g-contrast">
             <div class="g-circle"></div>
             <ul class="g-bubbles">
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
               <li></li>
               <li></li>
               <li></li>
@@ -121,7 +118,7 @@ export default {
   }
 }
 </script>
-<style lang="less" scoped>
+<style lang="scss" scoped>
 .none {
   display: none;
 }
@@ -136,93 +133,6 @@ export default {
   }
   .left {
     flex: 1;
-  }
-  .battery-box {
-    height: 240px;
-    .battery-img {
-      position: relative;
-      height: 90px;
-      width: 200px;
-      box-sizing: border-box;
-      box-shadow: 0 0 5px 2px #ddd;
-      border-radius: 5px 10px 10px 5px;
-      .header {
-        height: 25px;
-        width: 10px;
-        background-color: #ccc;
-        position: absolute;
-        top: 50%;
-        right: -10px;
-        transform: translate(0, -50%);
-        border-radius: 0 5px 5px 0;
-      }
-      .percent-bg {
-        position: relative;
-        height: 100%;
-        transition: all 1s;
-        border-radius: 6px;
-        background: linear-gradient(to right, #67C23A 0%, rgb(21, 226, 21) 100%);
-        box-shadow: 0 14px 28px rgba(33, 150, 243, 0), 0 10px 10px rgba(9, 188, 215, 0.08);
-        filter: hue-rotate(10deg);
-      }
-      .percent-bg-copy {
-        border-radius: 6px;
-        overflow: hidden;
-        position: absolute;
-        left: 0;
-        right: 0;
-        height: 100%;
-        .wave {
-          position: absolute;
-          background: rgba(255, 255, 255, .8);
-          border-radius: 45% 47% 44% 42%;
-          height: 200px;
-          width: 200px;
-          right: -190px;
-          top: 50%;
-          transform: translate(0, -50%);
-          z-index: 1;
-          animation: wave 13s linear infinite;
-          &:nth-child(2) {
-            top: 40%;
-            border-radius: 42% 46% 43% 47%;
-            transform: translate(0, -50%) rotate(-135deg);
-          }
-          &:nth-child(3) {
-            top: 60%;
-            border-radius: 46% 46% 43% 40%;
-            transform: translate(0, -50%) rotate(135deg);
-          }
-        }
-      }
-    }
-    .item {
-      padding: 15px 0;
-    }
-    .battery-value {
-      font-size: 18px;
-      font-weight: bold;
-      text-align: center;
-    }
-    .battery-power,.battery-status {
-      width: 100%;
-      text-indent: 25px;
-    }
-    .battery-power {
-      &:hover {
-        cursor: pointer;
-      }
-      .num {
-        color: #FDB201;
-        margin: 0 5px 0 15px;
-        font-size: 18px;
-      }
-    }
-    .battery-status {
-      .status {
-        margin: 0 5px 0 15px;
-      }
-    }
   }
 }
 .my-pg {
@@ -245,48 +155,47 @@ export default {
     }
   }
 }
-@keyframes wave {
-  100% {
-    transform: translate(0, -50%) rotate(720deg);
-  }
-}
 .g-number {
   position: absolute;
-  width: 300px;
-  top: 27%;
+  left: 50%;
+  transform: translate(-50%);
+  // width: 300px;
+  top: 30px;
   text-align: center;
-  font-size: 32px;
+  font-size: 20px;
   z-index: 10;
-  color: #fff;
+  color: #00ff6f;
+  filter: contrast(15) hue-rotate(0);
+  animation: hueRotate 10s infinite linear;
 }
 .g-container {
   position: relative;
-  width: 300px;
-  height: 400px;
+  width: 210px;
+  height: 240px;
   margin: auto;
 }
 .g-contrast {
   filter: contrast(15) hue-rotate(0);
-  width: 300px;
-  height: 400px;
-  background-color: #000;
+  width: 100%;
+  height: 100%;
   overflow: hidden;
+  background-color: #fff;
   animation: hueRotate 10s infinite linear;
 }
 .g-circle {
   position: relative;
-  width: 300px;
-  height: 300px;
+  width: 210px;
+  height: 144px;
   box-sizing: border-box;
   filter: blur(8px);
   &::after {
     content: "";
     position: absolute;
-    top: 40%;
+    top: 50%;
     left: 50%;
     transform: translate(-50%, -50%) rotate(0);
-    width: 200px;
-    height: 200px;
+    width: 144px;
+    height: 144px;
     background-color: #00ff6f;
     border-radius: 42% 38% 62% 49% / 45%;
     animation: rotate 10s infinite linear;
@@ -294,13 +203,13 @@ export default {
   &::before {
     content: "";
     position: absolute;
-    width: 176px;
-    height: 176px;
-    top: 40%;
+    width: 120px;
+    height: 120px;
+    top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     border-radius: 50%;
-    background-color: #000;
+    background-color: #fff;
     z-index: 10;
   }
 }
@@ -308,10 +217,12 @@ export default {
   position: absolute;
   left: 50%;
   bottom: 0;
-  width: 100px;
-  height: 40px;
+  width: 80px;
+  height: 20px;
+  margin: 0;
+  padding: 0;
   transform: translate(-50%, 0);
-  border-radius: 100px 100px 0 0;
+  border-radius: 20px 20px 0 0;
   background-color: #00ff6f;
   filter: blur(5px);
 }
@@ -319,19 +230,19 @@ li {
   position: absolute;
   border-radius: 50%;
   background: #00ff6f;
+  list-style: none;
 }
-// @for $i from 0 through 15 {
-//   li:nth-child(#{$i}) {
-//     $width: 15 + random(15) + px;
-//     left: 15 + random(70) + px;
-//     top: 50%;
-//     transform: translate(-50%, -50%);
-//     width: $width;
-//     height: $width;
-//     animation: moveToTop #{random(6) + 3}s ease-in-out -#{random(5000)/1000}s infinite;
-//   }
-// }
-
+@for $i from 1 through 10 {
+  li:nth-child(#{$i}) {
+    $width: 10 + random(10) + px;
+    left: random(60) + px;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: $width;
+    height: $width;
+    animation: moveToTop #{random(4) + 1}s ease-in-out -#{random(4000)/1000}s infinite;
+  }
+}
 @keyframes rotate {
   50% {
     border-radius: 45% / 42% 38% 58% 49%;
@@ -347,7 +258,7 @@ li {
   }
   100% {
     opacity: .1;
-    transform: translate(-50%, -180px);
+    transform: translate(-50%, -85px);
   }
 }
 
