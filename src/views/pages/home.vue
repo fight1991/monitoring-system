e1748cb80344cf277b0c9c99de8d2f4e73801e93
fight1991 <359193813@qq.com>
2020-09-27 11:29:35 +0800

    首页动画修改

--- src/views/pages/home.vue
+++ src/views/pages/home.vue
@@ -30,19 +30,26 @@ export default {
       font-size: 20px;
       position: absolute;
       left: 50%;
-      top: 10px;
+      top: 0px;
       transform: translate(-50%,0);
       color: #fff;
       letter-spacing: 3px;
       text-align: center;
       text-shadow: 1px 1px greenyellow, -1px -1px greenyellow,1px -1px greenyellow, -1px 1px greenyellow;
-      animation: welcomeAnimate 5s linear infinite;
+      animation: welcomeAnimate 5s ease-in-out infinite;
       filter: contrast(15) hue-rotate(0);
     }
   }
 }
 @keyframes welcomeAnimate {
+  0% {
+    transform: translate(-50%, 10px);
+  }
+  50% {
+    transform: translate(-50%, 0px);
+  }
   100% {
+    transform: translate(-50%, 10px);
     filter: contrast(15) hue-rotate(360deg);
   }
 }
