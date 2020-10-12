ece4fbbeb6dc3a30043554452168f6cbe08e0b78
fight1991 <359193813@qq.com>
2020-10-12 10:43:46 +0800

    封装请求完成

--- src/net/netConfig.js
+++ src/net/netConfig.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import interceptors from './interceptors'
+import storage from '@/util/storage'
+import store from '@/store'
 import { startLoading, closeLoading } from '@/util'
 let {
   onRequestResolve,
@@ -8,7 +10,8 @@ let {
   onResponseReject
 } = interceptors
 
-export class MethodBase {
+// axios构造实例类
+class InitAxios {
   constructor (baseURL) {
     this.instance = axios.create({
       // 开发环境中以/api开头的接口需要设置代理
@@ -19,21 +22,65 @@ export class MethodBase {
     this.instance.interceptors.response.use(onResponseResolve, onResponseReject)
   }
 }
-export class MethodAll {
+// 处理所有请求类
+export class AllFetch {
   constructor (params) {
     this.params = params
   }
-  async $all (store, isLoad, globalLoading) {
+  async requestFunc (isLoad, globalLoading) {
     let res = null
     let tabId = store.state.tab.currentTab
+    let loadingStatus = globalLoading || store.state.isGlobalLoading
     try {
-      isLoad && startLoading(store, tabId, globalLoading)
+      isLoad && startLoading(store, tabId, loadingStatus)
       res = await Promise.all(this.params)
-      isLoad && closeLoading(store, tabId, globalLoading)
+      isLoad && closeLoading(store, tabId, loadingStatus)
       return res
     } catch (err) {
-      isLoad && closeLoading(store, tabId, globalLoading)
+      isLoad && closeLoading(store, tabId, loadingStatus)
       return false
+    }
+  }
+}
+// 得到get/post等请求实例
+export class CommonFetch {
+  constructor (methodType) {
+    console.log(methodType)
+    this.methodType = methodType
+  }
+  async requestFunc ({ url, data, isLoad, checkParams, globalLoading }) {
+    // 设置入参
+    console.log(this)
+    let params = this.methodType === 'get' ? { params: data } : data
+    // 设置请求类型
+    let method = this.methodType === 'get' ? 'get' : 'post'
+    // 如果是上传文件 重新配置header选项和服务器地址
+    let config = {
+      headerOptions: this.methodType === 'upload' ? { headers: { 'Content-Type': 'multipart/form-data' } } : {},
+      serverUrl: this.methodType === 'upload' ? process.env.VUE_APP_FILE : process.env.VUE_APP_API
+    }
+    // 无论resolve还是reject都返回一个结果
+    let tabId = store.state.tab.currentTab
+    let loadingStatus = globalLoading || store.state.isGlobalLoading
+    try {
+      if (checkParams) { // 检查本地是否有缓存
+        if (storage.getStorage(checkParams)) {
+          return { result: storage.getStorage(checkParams) }
+        }
+      }
+      isLoad && startLoading(store, tabId, loadingStatus)
+      let { instance } = new InitAxios(config.serverUrl)
+      let res = await instance[method](url, params, config.headerOptions)
+      isLoad && closeLoading(store, tabId, loadingStatus)
+      if (res.errno === store.state.successCode) {
+        checkParams && storage.setStorage(checkParams, res.result)
+        return { result: res.result || true }
+      }
+      return { other: res }
+    } catch (err) {
+      // console.log(err)
+      isLoad && isLoad && closeLoading(store, tabId)
+      return { error: err }
     }
   }
 }
--- src/net/netMethods.js
+++ src/net/netMethods.js
@@ -1,96 +1,23 @@
 
-import store from '@/store'
-import storage from '@/util/storage'
-import { bussinessBundle, startLoading, closeLoading } from '@/util'
-import { MethodBase, MethodAll } from './netConfig'
-let { instance: commonInstance } = new MethodBase(process.env.VUE_APP_API)
-let { instance: upLoadInstance } = new MethodBase(process.env.VUE_APP_FILE)
+import { CommonFetch, AllFetch } from './netConfig'
+
+let fetchGet = new CommonFetch('get')
+let fetchPost = new CommonFetch('post')
+let fetchUpload = new CommonFetch('upload')
 
 const requests = {
-  // 返回 promise
-  async $axios ({ url, data = {}, method = 'get', isLoad = true, checkParams, globalLoading }) {
-    let params = method === 'get' ? { params: data } : data
-    // 无论resolve还是reject都返回一个结果
-    let tabId = store.state.tab.currentTab
-    let loadingStatus = globalLoading || store.state.isGlobalLoading
-    try {
-      if (checkParams) { // 检查本地是否有缓存
-        if (storage.getStorage(checkParams)) {
-          return { result: storage.getStorage(checkParams) }
-        }
-      }
-      isLoad && startLoading(store, tabId, loadingStatus)
-      let res = await commonInstance[method](url, params)
-      isLoad && closeLoading(store, tabId, loadingStatus)
-      if (res.errno === store.state.successCode) {
-        checkParams && storage.setStorage(checkParams, res.result)
-        return { result: res.result || true }
-      }
-      return { other: res }
-    } catch (err) {
-      // console.log(err)
-      isLoad && isLoad && closeLoading(store, tabId)
-      return { error: err }
-    }
+  $get ({ url, data = {}, isLoad = true, checkParams, globalLoading }) {
+    return fetchGet.requestFunc({ url, data, isLoad, checkParams, globalLoading })
+  },
+  $post ({ url, data = {}, isLoad = true, checkParams, globalLoading }) {
+    return fetchPost.requestFunc({ url, data, isLoad, checkParams, globalLoading })
+  },
+  $upload ({ url, data = {}, isLoad = true, checkParams, globalLoading }) {
+    return fetchUpload.requestFunc({ url, data, isLoad, checkParams, globalLoading })
   },
   // 多个并发请求
-  $all: {
-    promise (promiseArr, isLoad = true, globalLoading) { // 入参为promise对象,处理并发请求 async修饰的函数返回promise
-      let loadingStatus = globalLoading || store.state.isGlobalLoading
-      return new MethodAll(promiseArr).$all(store, isLoad, loadingStatus)
-    },
-    url (urlArr, isLoad = true, globalLoading) {
-      let loadingStatus = globalLoading || store.state.isGlobalLoading
-      return new MethodAll(urlArr.map(v => commonInstance.get(v))).$all(store, isLoad, loadingStatus)
-    }
-  },
-  // 自定义请求
-  $get ({ url, data = {}, success, other, error, isLoad = true, globalLoading }) {
-    let tabId = store.state.tab.currentTab
-    let loadingStatus = globalLoading || store.state.isGlobalLoading
-    isLoad && startLoading(store, tabId, loadingStatus)
-    commonInstance.get(url, {
-      params: data
-    }).then(res => {
-      bussinessBundle(res, other, success, store)
-    }).catch(err => {
-      if (process.env.NODE_ENV === 'development') console.log('api is ' + url, err)
-      error && error(err)
-    }).finally(() => {
-      isLoad && closeLoading(store, tabId, loadingStatus)
-    })
-  },
-  $post ({ url, data = {}, success, other, error, isLoad = true, globalLoading }) {
-    let tabId = store.state.tab.currentTab
-    let loadingStatus = globalLoading || store.state.isGlobalLoading
-    isLoad && startLoading(store, tabId, loadingStatus)
-    commonInstance.post(url, data)
-      .then(res => {
-        bussinessBundle(res, other, success, store)
-      })
-      .catch(err => {
-        if (process.env.NODE_ENV === 'development') console.log('api is ' + url, err)
-        error && error(err)
-      })
-      .finally(() => {
-        isLoad && closeLoading(store, tabId, loadingStatus)
-      })
-  },
-  $upload ({ url, data = {}, success, error, isLoad = true, globalLoading }) {
-    let tabId = store.state.tab.currentTab
-    let loadingStatus = globalLoading || store.state.isGlobalLoading
-    isLoad && startLoading(store, tabId, loadingStatus)
-    upLoadInstance.post(url, data, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    }).then(res => {
-      success && success(res)
-    }).catch(err => {
-      error && error(err)
-    }).finally(() => {
-      isLoad && closeLoading(store, tabId, loadingStatus)
-    })
+  $all (promiseArr, isLoad = true, globalLoading) { // 入参为promise对象,处理并发请求 async修饰的函数返回promise
+    return new AllFetch(promiseArr).requestFunc(isLoad, globalLoading)
   }
 }
 
